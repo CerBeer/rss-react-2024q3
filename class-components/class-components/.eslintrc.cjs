6c5f646067d86b2b73432b7e1365aefb11643240
CerBeer <ocelopp@gmail.com>
2024-07-04 00:08:25 +0300

    feat: update configurations

--- class-components/class-components/.eslintrc.cjs
+++ class-components/class-components/.eslintrc.cjs
@@ -2,17 +2,44 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    "airbnb" ,
+    "airbnb-typescript" ,
+    "airbnb/hooks",
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    "plugin:react/recommended",
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    "prettier"
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts', 'coverage'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['react-refresh', "react",
+    "@typescript-eslint",
+    "prettier"
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/react-in-jsx-scope': 0,
+    "react/jsx-props-no-spreading": "off",
+    "react/require-default-props": "off",
+  },
+  settings: {
+    "import/resolver": {
+      "node": {
+        "paths": ["public"]
+      }
+    },
   },
 }
