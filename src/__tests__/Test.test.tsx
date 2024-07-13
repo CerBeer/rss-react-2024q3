a3bb78f0ac2a4236911bba65dcefa6bb3dbacd02
CerBeer <ocelopp@gmail.com>
2024-07-13 13:35:58 +0300

    refactor: remove unused test

--- src/__tests__/Test.test.tsx
+++ src/__tests__/Test.test.tsx
@@ -1,12 +1,5 @@
 import { expect, test } from "vitest";
-import { render, screen } from "@testing-library/react";
-import App from "../App";
 
 test("demo", () => {
   expect(true).toBe(true);
-});
-
-test("get App markdown", () => {
-  render(<App />);
-  screen.debug();
 });
